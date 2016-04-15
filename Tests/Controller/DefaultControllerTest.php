be4960f85a3942fade2be155de61279712a5aa60
montu <montu.pepavanshi@brainvire.com>
2016-04-15 16:12:50 +0530

    create functional test for the finder class

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -4,14 +4,30 @@ namespace BvTestBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-        $client = static::createClient();
+class DefaultControllerTest extends WebTestCase {
+    
+    private $client = null;
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    public function setUp() {
+        $this->client = static::createClient();
     }
+    
+    // Functional test case for the finder method..
+    public function testBviFinderTest(){
+        
+        // call the URL
+        $crawler = $this->client->request('POST', '/default/index');
+        $this->client->getRequest()->attributes->get('_controller');
+        
+        // compare the both the URLs
+        $this->assertEquals('BvTestBundle\Controller\DefaultController::indexAction', $this->client->getRequest()->attributes->get('_controller'));
+        
+        // Submit the form using user defined value..
+        $form = $crawler->selectButton('submitButton')->form(array(
+            'search' => 'BVI'
+        ));
+        $crawler = $this->client->submit($form);
+        
+    }
+
 }
